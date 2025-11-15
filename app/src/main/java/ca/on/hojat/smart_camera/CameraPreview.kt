f9dadf94af72a26a5f14d4941a9b6df0a55b4d9f
hojat72elect <hojat72elect@gmail.com>
2025-11-15 09:19:36 -0500

    Improved the quality of the shutter button.

--- app/src/main/java/ca/on/hojat/smart_camera/CameraPreview.kt
+++ app/src/main/java/ca/on/hojat/smart_camera/CameraPreview.kt
@@ -14,11 +14,16 @@ import androidx.camera.core.ImageCaptureException
 import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.camera.view.PreviewView
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsPressedAsState
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -27,7 +32,10 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.compose.LocalLifecycleOwner
@@ -76,12 +84,25 @@ fun CameraPreview(modifier: Modifier = Modifier) {
             )
         }
 
-        Button(
-            onClick = { takePhoto(context, imageCapture) },
-            modifier = Modifier.align(Alignment.BottomCenter)
-        ) {
-            Text(text = "Capture")
-        }
+        val interactionSource = remember { MutableInteractionSource() }
+        val isPressed by interactionSource.collectIsPressedAsState()
+        val alpha by animateFloatAsState(targetValue = if (isPressed) 0.5f else 1f, label = "alpha")
+
+        Box(
+            modifier = Modifier
+                .align(Alignment.BottomCenter)
+                .padding(bottom = 8.dp)
+                .size(72.dp)
+                .graphicsLayer {
+                    this.alpha = alpha
+                }
+                .background(Color.White, CircleShape)
+                .clickable(
+                    interactionSource = interactionSource,
+                    indication = null, // No ripple
+                    onClick = { takePhoto(context, imageCapture) }
+                )
+        )
     }
 }
 
