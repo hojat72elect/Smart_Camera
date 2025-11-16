1e28e1f39c4419660d8e4ab0082bf0c18c989038
hojat72elect <hojat72elect@gmail.com>
2025-11-16 17:37:58 -0500

    Extracted the logic for taking the photo, to a separate file.

--- app/src/main/java/ca/on/hojat/smart_camera/CameraPreview.kt
+++ app/src/main/java/ca/on/hojat/smart_camera/CameraPreview.kt
@@ -1,19 +1,12 @@
 package ca.on.hojat.smart_camera
 
 import android.Manifest
-import android.content.ContentValues
-import android.content.Context
 import android.media.MediaActionSound
-import android.net.Uri
-import android.os.Build
-import android.provider.MediaStore
 import android.widget.Toast
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.camera.core.CameraSelector
 import androidx.camera.core.ImageCapture
-import androidx.camera.core.ImageCaptureException
-import androidx.camera.core.ImageProxy
 import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.camera.view.PreviewView
@@ -52,14 +45,8 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.compose.LocalLifecycleOwner
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
-import java.nio.ByteBuffer
-import java.text.SimpleDateFormat
-import java.util.Locale
 
 @Composable
 fun CameraPreview(modifier: Modifier = Modifier) {
@@ -181,7 +168,7 @@ fun CameraPreview(modifier: Modifier = Modifier) {
                     interactionSource = interactionSource,
                     indication = null, // No ripple
                     onClick = {
-                        takePhoto(
+                        takePhotoUseCase(
                             context = context,
                             scope = scope,
                             imageCapture = imageCapture,
@@ -214,75 +201,4 @@ fun CameraPreview(modifier: Modifier = Modifier) {
                 )
         )
     }
-}
-
-fun takePhoto(
-    context: Context,
-    scope: CoroutineScope,
-    imageCapture: ImageCapture,
-    isFlashOn: Boolean,
-    onPhotoCaptured: () -> Unit,
-    onSuccess: (Uri) -> Unit,
-    onError: (ImageCaptureException) -> Unit
-) {
-    imageCapture.flashMode = if (isFlashOn) ImageCapture.FLASH_MODE_ON else ImageCapture.FLASH_MODE_OFF
-
-    imageCapture.takePicture(
-        ContextCompat.getMainExecutor(context),
-        object : ImageCapture.OnImageCapturedCallback() {
-            override fun onCaptureSuccess(image: ImageProxy) {
-                onPhotoCaptured()
-                scope.launch(Dispatchers.IO) {
-                    val buffer: ByteBuffer = image.planes[0].buffer
-                    val bytes = ByteArray(buffer.remaining())
-                    buffer.get(bytes)
-
-                    val name =
-                        SimpleDateFormat("yyyyMMdd_HHmmss", Locale.US).format(System.currentTimeMillis())
-                    val contentValues = ContentValues().apply {
-                        put(MediaStore.MediaColumns.DISPLAY_NAME, name)
-                        put(MediaStore.MediaColumns.MIME_TYPE, "image/jpeg")
-                        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.P) {
-                            put(MediaStore.Images.Media.RELATIVE_PATH, "Pictures/Smart-Camera")
-                        }
-                    }
-
-                    var uri: Uri? = null
-                    try {
-                        val resolver = context.contentResolver
-                        uri = resolver.insert(
-                            MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
-                            contentValues
-                        )
-                        if (uri == null) {
-                            throw Exception("Failed to create new MediaStore entry.")
-                        }
-                        resolver.openOutputStream(uri)?.use {
-                            it.write(bytes)
-                        }
-                        withContext(Dispatchers.Main) {
-                            onSuccess(uri)
-                        }
-                    } catch (exc: Exception) {
-                        uri?.let { context.contentResolver.delete(it, null, null) }
-                        withContext(Dispatchers.Main) {
-                            onError(
-                                ImageCaptureException(
-                                    ImageCapture.ERROR_FILE_IO,
-                                    "Failed to save image",
-                                    exc
-                                )
-                            )
-                        }
-                    } finally {
-                        image.close()
-                    }
-                }
-            }
-
-            override fun onError(exception: ImageCaptureException) {
-                onError(exception)
-            }
-        }
-    )
 }
---
+++ app/src/main/java/ca/on/hojat/smart_camera/takePhotoUseCase.kt
@@ -0,0 +1,89 @@
+package ca.on.hojat.smart_camera
+
+import android.content.ContentValues
+import android.content.Context
+import android.net.Uri
+import android.os.Build
+import android.provider.MediaStore
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.ImageCaptureException
+import androidx.camera.core.ImageProxy
+import androidx.core.content.ContextCompat
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.nio.ByteBuffer
+import java.text.SimpleDateFormat
+import java.util.Locale
+
+fun takePhotoUseCase(
+    context: Context,
+    scope: CoroutineScope,
+    imageCapture: ImageCapture,
+    isFlashOn: Boolean,
+    onPhotoCaptured: () -> Unit,
+    onSuccess: (Uri) -> Unit,
+    onError: (ImageCaptureException) -> Unit
+) {
+    imageCapture.flashMode = if (isFlashOn) ImageCapture.FLASH_MODE_ON else ImageCapture.FLASH_MODE_OFF
+
+    imageCapture.takePicture(
+        ContextCompat.getMainExecutor(context),
+        object : ImageCapture.OnImageCapturedCallback() {
+            override fun onCaptureSuccess(image: ImageProxy) {
+                onPhotoCaptured()
+                scope.launch(Dispatchers.IO) {
+                    val buffer: ByteBuffer = image.planes[0].buffer
+                    val bytes = ByteArray(buffer.remaining())
+                    buffer.get(bytes)
+
+                    val name =
+                        SimpleDateFormat("yyyyMMdd_HHmmss", Locale.US).format(System.currentTimeMillis())
+                    val contentValues = ContentValues().apply {
+                        put(MediaStore.MediaColumns.DISPLAY_NAME, name)
+                        put(MediaStore.MediaColumns.MIME_TYPE, "image/jpeg")
+                        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.P) {
+                            put(MediaStore.Images.Media.RELATIVE_PATH, "Pictures/Smart-Camera")
+                        }
+                    }
+
+                    var uri: Uri? = null
+                    try {
+                        val resolver = context.contentResolver
+                        uri = resolver.insert(
+                            MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                            contentValues
+                        )
+                        if (uri == null) {
+                            throw Exception("Failed to create new MediaStore entry.")
+                        }
+                        resolver.openOutputStream(uri)?.use {
+                            it.write(bytes)
+                        }
+                        withContext(Dispatchers.Main) {
+                            onSuccess(uri)
+                        }
+                    } catch (exc: Exception) {
+                        uri?.let { context.contentResolver.delete(it, null, null) }
+                        withContext(Dispatchers.Main) {
+                            onError(
+                                ImageCaptureException(
+                                    ImageCapture.ERROR_FILE_IO,
+                                    "Failed to save image",
+                                    exc
+                                )
+                            )
+                        }
+                    } finally {
+                        image.close()
+                    }
+                }
+            }
+
+            override fun onError(exception: ImageCaptureException) {
+                onError(exception)
+            }
+        }
+    )
+}
