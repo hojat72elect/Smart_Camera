7eaa45c0f6d04ca2a2f2cc074f5615c0cfab79f1
hojat72elect <hojat72elect@gmail.com>
2026-05-12 00:27:38 -0400

    Cleaned up.

--- SmartCamera/App.tsx
+++ SmartCamera/App.tsx
@@ -1,20 +1,20 @@
 import React from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View } from 'react-native';
+import {StatusBar} from 'expo-status-bar';
+import {StyleSheet, View} from 'react-native';
 import CameraScreen from './src/components/CameraScreen';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <CameraScreen />
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <CameraScreen/>
+            <StatusBar style="auto"/>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#000',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#000',
+    },
 });
